0362f81038f8515db03431766d2f6b9b5a494b1a
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-04-23 15:48:48 -0400

    Now echoing output in terminal

--- main.php
+++ main.php
@@ -2,3 +2,15 @@
 
 //open a text file
 //for each line, spit out the contents
+
+$handle = fopen("dracula.txt", 'r');
+if($handle){
+    while(($line = fgets($handle)) !== false){
+        //do something with the line
+        echo $line;
+    }
+    fclose($handle);
+} else{
+    echo "There was an error";
+}
+echo "\n";
